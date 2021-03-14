70f39a26ab2066335b18177468f8f5ac6f140f6d
Tobias Reiter <t.reiter1@live.de>
2021-03-14 17:29:18 +0100

    code format

--- include/rayTracer/rtTrace.hpp
+++ include/rayTracer/rtTrace.hpp
@@ -2,109 +2,90 @@
 #define RT_TRACE_HPP
 
 #include <iostream>
-#include <rti/device.hpp>
 #include <lsDomain.hpp>
 #include <lsToDiskMesh.hpp>
 #include <psSmartPointer.hpp>
+#include <rti/device.hpp>
 
 using NumericType = float;
 
-template <class particle, class reflection, int D>
-class rtTrace
-{
+template <class particle, class reflection, int D> class rtTrace {
 public:
-    typedef psSmartPointer<rti::device<NumericType, particle, reflection>> rtDeviceType;
+  typedef psSmartPointer<rti::device<NumericType, particle, reflection>>
+      rtDeviceType;
 
 private:
-    rtDeviceType rtiDevice = nullptr;
+  rtDeviceType rtiDevice = nullptr;
 
 public:
-    rtTrace()
-    {
-        rtiDevice = rtDeviceType::New();
-    };
+  rtTrace() { rtiDevice = rtDeviceType::New(); };
 
-    rtTrace(lsSmartPointer<lsDomain<NumericType, D>> passedlsDomain,
-            const NumericType discRadius)
-    {
-        rtiDevice = rtDeviceType::New();
-        auto mesh = lsSmartPointer<lsMesh<NumericType>>::New();
-        lsToDiskMesh<NumericType, D>(passedlsDomain, mesh).apply();
-        auto points = mesh.get()->getNodes();
-        auto normals = *mesh.get()->getVectorData("Normals");
+  rtTrace(lsSmartPointer<lsDomain<NumericType, D>> passedlsDomain,
+          const NumericType discRadius) {
+    rtiDevice = rtDeviceType::New();
+    auto mesh = lsSmartPointer<lsMesh<NumericType>>::New();
+    lsToDiskMesh<NumericType, D>(passedlsDomain, mesh).apply();
+    auto points = mesh.get()->getNodes();
+    auto normals = *mesh.get()->getVectorData("Normals");
 
-        rtiDevice.get()->set_points(points);
-        rtiDevice.get()->set_normals(normals);
-        rtiDevice.get()->set_grid_spacing(discRadius);
-        rtiDevice.get()->set_number_of_rays(points.size() * 100);
-    }
+    rtiDevice.get()->set_points(points);
+    rtiDevice.get()->set_normals(normals);
+    rtiDevice.get()->set_grid_spacing(discRadius);
+    rtiDevice.get()->set_number_of_rays(points.size() * 100);
+  }
 
-    rtTrace(std::vector<NumericType> &points, std::vector<NumericType> &normals,
-            const NumericType discRadius)
-    {
-        assert(points.size() == normals.size() && "Assumption");
+  rtTrace(std::vector<NumericType> &points, std::vector<NumericType> &normals,
+          const NumericType discRadius) {
+    assert(points.size() == normals.size() && "Assumption");
 
-        rtiDevice = rtDeviceType::New();
-        rtiDevice.get()->set_points(points);
-        rtiDevice.get()->set_normals(normals);
-        rtiDevice.get()->set_grid_spacing(discRadius);
-        rtiDevice.get()->set_number_of_rays(points.size() * 100);
-    }
+    rtiDevice = rtDeviceType::New();
+    rtiDevice.get()->set_points(points);
+    rtiDevice.get()->set_normals(normals);
+    rtiDevice.get()->set_grid_spacing(discRadius);
+    rtiDevice.get()->set_number_of_rays(points.size() * 100);
+  }
 
-    void apply()
-    {
-        rtiDevice.get()->run();
-    }
+  void apply() { rtiDevice.get()->run(); }
 
-    std::vector<NumericType> getMcEstimates()
-    {
-        return rtiDevice.get()->get_mc_estimates();
-    }
+  std::vector<NumericType> getMcEstimates() {
+    return rtiDevice.get()->get_mc_estimates();
+  }
 
-    std::vector<NumericType> getHitCounts()
-    {
-        return rtiDevice.get()->get_hit_counts();
-    }
+  std::vector<NumericType> getHitCounts() {
+    return rtiDevice.get()->get_hit_counts();
+  }
 
-    void setPoints(std::vector<NumericType> &points)
-    {
-        rtiDevice.get()->set_points(points);
-    }
+  void setPoints(std::vector<NumericType> &points) {
+    rtiDevice.get()->set_points(points);
+  }
 
-    void setNormals(std::vector<NumericType> &normals)
-    {
-        rtiDevice.get()->set_points(normals);
-    }
+  void setNormals(std::vector<NumericType> &normals) {
+    rtiDevice.get()->set_points(normals);
+  }
 
-    void setDiscRadius(const NumericType discRadius)
-    {
-        rtiDevice.get()->set_grid_spacing(discRadius);
-    }
+  void setDiscRadius(const NumericType discRadius) {
+    rtiDevice.get()->set_grid_spacing(discRadius);
+  }
 
-    void setPowerCosineDirection(const NumericType exp)
-    {
-        auto direction = rti::ray::power_cosine_direction_z<NumericType>{exp};
-        rtiDevice.get()->set(direction);
-    }
+  void setPowerCosineDirection(const NumericType exp) {
+    auto direction = rti::ray::power_cosine_direction_z<NumericType>{exp};
+    rtiDevice.get()->set(direction);
+  }
 
-    void setNumOfRays(size_t num)
-    {
-        rtiDevice.get()->set_number_of_rays(num);
-    }
+  void setNumOfRays(size_t num) { rtiDevice.get()->set_number_of_rays(num); }
 
-    void setDomain(lsSmartPointer<lsDomain<NumericType, D>> passedlsDomain,
-                   const NumericType discRadius)
-    {
-        auto mesh = lsSmartPointer<lsMesh<NumericType>>::New();
-        lsToDiskMesh<NumericType, D>(passedlsDomain, mesh).apply();
-        auto points = mesh.get()->getNodes();
-        auto normals = *mesh.get()->getVectorData("Normals");
+  void setDomain(lsSmartPointer<lsDomain<NumericType, D>> passedlsDomain,
+                 const NumericType discRadius) {
+    auto mesh = lsSmartPointer<lsMesh<NumericType>>::New();
+    lsToDiskMesh<NumericType, D>(passedlsDomain, mesh).apply();
+    auto points = mesh.get()->getNodes();
+    auto normals = *mesh.get()->getVectorData("Normals");
 
-        rtiDevice.get()->set_points(points);
-        rtiDevice.get()->set_normals(normals);
-        rtiDevice.get()->set_grid_spacing(discRadius);
-        rtiDevice.get()->set_number_of_rays(points.size() * 100);
-    }
+    rtiDevice.get()->set_points(points);
+    rtiDevice.get()->set_normals(normals);
+    rtiDevice.get()->set_grid_spacing(discRadius);
+    rtiDevice.get()->set_number_of_rays(points.size() * 100);
+  }
 };
 
 #endif // RT_TRACE_HPP
