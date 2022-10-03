7ed606949a766c62b3cacd811003edb61f24cafd
Tobias Reiter <t.reiter1@live.de>
2022-10-03 10:29:39 +0200

    Change format script to only format include and Examples

--- format-project.sh
+++ format-project.sh
@@ -3,4 +3,5 @@
 # note: if you aliased clang-format, invoke this script using
 # bash -i format-project.sh
 
-find `pwd` -iname "*.hpp" -o -iname "*.cpp" | while read -r i; do clang-format -i "$i"; done
+find include/ -iname "*.hpp" -o -iname "*.cpp" | while read -r i; do clang-format -i "$i"; done
+find Examples/ -iname "*.hpp" -o -iname "*.cpp" | while read -r i; do clang-format -i "$i"; done
