e6b5385ee3acafa4ab2f5242e492cf20097c97a2
Tobias Reiter <t.reiter1@live.de>
2025-02-19 10:10:52 +0100

    Add IBE with repeposition example

---
+++ examples/ionBeamEtching/ionBeamEtching.cpp
@@ -0,0 +1,74 @@
+#include <geometries/psMakeTrench.hpp>
+#include <models/psDirectionalEtching.hpp>
+#include <models/psIonBeamEtching.hpp>
+
+#include <psProcess.hpp>
+#include <psUtils.hpp>
+
+using namespace viennaps;
+
+int main(int argc, char *argv[]) {
+  using NumericType = double;
+  constexpr int D = 2;
+  Logger::setLogLevel(LogLevel::INTERMEDIATE);
+
+  // Parse the parameters
+  utils::Parameters params;
+  if (argc > 1) {
+    params.readConfigFile(argv[1]);
+  } else {
+    std::cout << "Usage: " << argv[0] << " <config file>" << std::endl;
+    return 1;
+  }
+
+  auto geometry = SmartPointer<Domain<NumericType, D>>::New();
+  MakeTrench<NumericType, D>(
+      geometry, params.get("gridDelta"), params.get("xExtent"),
+      params.get("yExtent"), params.get("trenchWidth"),
+      params.get("maskHeight"), 0.0 /* tapering angle */, 0.0 /* base height */,
+      true /*periodic*/, true /*make mask*/, Material::Si)
+      .apply();
+
+  {
+    // etch trench
+    Vec3D<NumericType> direction{0., 0., 0.};
+    direction[D - 1] = -1.;
+    auto model =
+        SmartPointer<DirectionalEtching<NumericType, D>>::New(direction, -1.0);
+    Process<NumericType, D>(geometry, model, params.get("trenchDepth")).apply();
+  }
+
+  // copy top layer to capture deposition
+  geometry->duplicateTopLevelSet(Material::Polymer);
+
+  IBEParameters<NumericType> ibeParams;
+  ibeParams.tiltAngle = params.get("angle");
+  ibeParams.exponent = params.get("exponent");
+
+  ibeParams.meanEnergy = params.get("meanEnergy");
+  ibeParams.sigmaEnergy = params.get("sigmaEnergy");
+  ibeParams.thresholdEnergy = params.get("thresholdEnergy");
+
+  ibeParams.redepositionRate = params.get("redepositionRate");
+  ibeParams.planeWaferRate = params.get("planeWaferRate");
+
+  auto model = SmartPointer<IonBeamEtching<NumericType, D>>::New(
+      std::vector<Material>{Material::Mask}, ibeParams);
+  Vec3D<NumericType> direction{0., 0., 0.};
+  direction[D - 1] = -std::cos(ibeParams.tiltAngle * M_PI / 180.);
+  direction[D - 2] = std::sin(ibeParams.tiltAngle * M_PI / 180.);
+  model->setPrimaryDirection(direction);
+
+  Process<NumericType, D> process;
+  process.setDomain(geometry);
+  process.setProcessModel(model);
+  process.setProcessDuration(params.get("processTime"));
+  process.setIntegrationScheme(
+      viennals::IntegrationSchemeEnum::LAX_FRIEDRICHS_2ND_ORDER);
+
+  geometry->saveHullMesh("initial");
+
+  process.apply();
+
+  geometry->saveHullMesh("final");
+}
