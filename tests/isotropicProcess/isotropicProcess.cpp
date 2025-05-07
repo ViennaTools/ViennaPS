0f88f8d4daaeda2f852ac9e8a47218e6fff8255c
reiter <t.reiter1@live.de>
2025-05-07 15:54:19 +0200

    Add material dependent rates for isotropic process

--- tests/isotropicProcess/isotropicProcess.cpp
+++ tests/isotropicProcess/isotropicProcess.cpp
@@ -56,8 +56,32 @@ template <class NumericType, int D> void RunTest() {
     VC_TEST_ASSERT(domain->getMaterialMap()->size() == 2);
     LSTEST_ASSERT_VALID_LS(domain->getLevelSets().back(), NumericType, D);
   }
+
+  {
+    auto domain = SmartPointer<Domain<NumericType, D>>::New();
+    MakeTrench<NumericType, D>(domain, 1., 10., 10., 2.5, 5., 10., 1., false,
+                               true, Material::Si)
+        .apply();
+
+    std::unordered_map<Material, NumericType> materialRates(
+        {{Material::Mask, -0.1}, {Material::Si, -1.}});
+    auto model =
+        SmartPointer<IsotropicProcess<NumericType, D>>::New(materialRates);
+
+    VC_TEST_ASSERT(model->getSurfaceModel());
+    VC_TEST_ASSERT(model->getVelocityField());
+    VC_TEST_ASSERT(model->getVelocityField()->getTranslationFieldOptions() ==
+                   0);
+
+    Process<NumericType, D>(domain, model, 2.).apply();
+
+    VC_TEST_ASSERT(domain->getLevelSets().size() == 2);
+    VC_TEST_ASSERT(domain->getMaterialMap());
+    VC_TEST_ASSERT(domain->getMaterialMap()->size() == 2);
+    LSTEST_ASSERT_VALID_LS(domain->getLevelSets().back(), NumericType, D);
+  }
 }
 
 } // namespace viennacore
 
-int main() { VC_RUN_ALL_TESTS }
+int main() { VC_RUN_2D_TESTS }
