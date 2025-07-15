bbda0b5a1ef701196455d81a5d0da1416f36e528
Tobias Reiter <t.reiter1@live.de>
2025-07-15 15:29:59 +0200

    Add mask to hole etch output

--- examples/holeEtching/testFluxes.py
+++ examples/holeEtching/testFluxes.py
@@ -23,15 +23,15 @@ vps.Length.setUnit("um")
 vps.Time.setUnit("min")
 
 # hole geometry parameters
-gridDelta = 0.03  # um
-extent = 3.0
+gridDelta = 0.018  # um
+extent = 1.5
 holeRadius = 0.175
 maskHeight = 1.2
 taperAngle = 1.193
 
 # fluxes
 ionFlux = [10.0, 10.0, 10.0, 10.0, 10.0]
-etchantFlux = [4.8e3, 4.5e3, 4e3, 3.5e3, 4e3]
+etchantFlux = [4.8e3, 4.5e3, 4e3, 3.5e3, 5e3]
 oxygenFlux = [3e2, 8e2, 2e3, 2.5e3, 0.0]
 A_O = [2, 2, 2, 1, 1]
 yo2 = [0.44, 0.5, 0.56, 0.62, 0]
@@ -71,7 +71,7 @@ for i in range(len(yo2)):
         holeDepth=0.0,
         maskHeight=maskHeight,
         maskTaperAngle=taperAngle,
-        # holeShape=vps.HoleShape.Half,
+        holeShape=vps.HoleShape.Half,
     ).apply()
 
     process = vps.Process()
@@ -92,4 +92,11 @@ for i in range(len(yo2)):
     process.setProcessModel(model)
     process.apply()
 
-    geometry.saveSurfaceMesh("hole_y{:.2f}_EO2.vtp".format(yo2[i]), True)
+    # save mask
+    mask = geometry.getLevelSets()[0]
+    mesh = vps.ls.Mesh()
+    vps.ls.ToSurfaceMesh(mask, mesh).apply()
+    vps.ls.VTKWriter(mesh, "mask_y{:.2f}.vtp".format(yo2[i])).apply()
+
+    geometry.saveSurfaceMesh("hole_y{:.2f}.vtp".format(yo2[i]), True)
+    geometry.saveVolumeMesh("hole_y{:.2f}".format(yo2[i]))
