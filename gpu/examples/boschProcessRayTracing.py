ce5b5baa2fe8e160779b04fc0c74204adfdf6bc7
reiter <t.reiter1@live.de>
2025-07-15 13:58:36 +0200

    Adjust bosch process example

--- gpu/examples/boschProcessRayTracing.py
+++ gpu/examples/boschProcessRayTracing.py
@@ -2,7 +2,7 @@
 # 3D Bosch process simulation using ray tracing
 # Replicates the results from:
 #   Ertl and Selberherr https://doi.org/10.1016/j.mee.2009.05.011
-# Execute: 
+# Execute:
 #   python boschProcessRayTracing.py -D 3 configBoschRayTracing.txt
 #####################################################################
 
@@ -55,9 +55,11 @@ depoModel.addNeutralParticle(params["stickingDep"])
 depoModel.addIonParticle(params["ionSourceExponent"])
 
 etchModel = vps.gpu.MultiParticleProcess()
-materialStickingEtch = {vps.Material.Si: params["stickingEtchSubs"],
-                        vps.Material.Mask: params["stickingEtchMask"],
-                        vps.Material.Polymer: params["stickingEtchPoly"]}
+materialStickingEtch = {
+    vps.Material.Si: params["stickingEtchSubs"],
+    vps.Material.Mask: params["stickingEtchMask"],
+    vps.Material.Polymer: params["stickingEtchPoly"],
+}
 etchModel.addNeutralParticle(materialStickingEtch, defaultStickingProbability=1.0)
 etchModel.addIonParticle(params["ionSourceExponent"])
 
@@ -66,32 +68,38 @@ A2c = 1e-8  # 1 angstrom = 1e-8 cm
 c2u = 1e4  # 1 cm = 1e4 micrometers
 
 # Rate calculations for the depo model
-alphaDep = params["alphaDep"] * (A2c ** 3)  # [cm^3/atom]
+alphaDep = params["alphaDep"] * (A2c**3)  # [cm^3/atom]
 ionDepRate = alphaDep * params["Flux_ionD"] * c2u  # [um/s]
-betaDep = params["betaDep"] * (A2c ** 3)  # [cm^3/atom]
+betaDep = params["betaDep"] * (A2c**3)  # [cm^3/atom]
 neuDepRate = betaDep * params["Flux_neuD"] * c2u  # [um/s]
 # Print the result
 print(f"Deposition cycle:")
 print(f"Poly deposit rate (ion, neutral): {ionDepRate:.3e} μm/s, {neuDepRate:.3e} μm/s")
 
 # Rate calculations for the etch model
-alphaPoly = params["alphaPoly"] * (A2c ** 3)  # [cm^3/atom]
-alphaSubs = params["alphaSubs"] * (A2c ** 3)  # [cm^3/atom]
-alphaMask = params["alphaMask"] * (A2c ** 3)  # [cm^3/atom]
+alphaPoly = params["alphaPoly"] * (A2c**3)  # [cm^3/atom]
+alphaSubs = params["alphaSubs"] * (A2c**3)  # [cm^3/atom]
+alphaMask = params["alphaMask"] * (A2c**3)  # [cm^3/atom]
 ionEtchRatePoly = alphaPoly * params["Flux_ionE"] * c2u  # [um/s]
 ionEtchRateSubs = alphaSubs * params["Flux_ionE"] * c2u  # [um/s]
 ionEtchRateMask = alphaMask * params["Flux_ionE"] * c2u  # [um/s]
-betaPoly = params["betaPoly"] * (A2c ** 3)  # [cm^3/atom]
-betaSubs = params["betaSubs"] * (A2c ** 3)  # [cm^3/atom]
-betaMask = params["betaMask"] * (A2c ** 3)  # [cm^3/atom]
+betaPoly = params["betaPoly"] * (A2c**3)  # [cm^3/atom]
+betaSubs = params["betaSubs"] * (A2c**3)  # [cm^3/atom]
+betaMask = params["betaMask"] * (A2c**3)  # [cm^3/atom]
 neuEtchRatePoly = betaPoly * params["Flux_neuE"] * c2u  # [um/s]
 neuEtchRateSubs = betaSubs * params["Flux_neuE"] * c2u  # [um/s]
 neuEtchRateMask = betaMask * params["Flux_neuE"] * c2u  # [um/s]
 # Print the result
 print(f"Etching cycle:")
-print(f"Mask etching rate (ion, neutral): {ionEtchRateMask:.3e} μm/s, {neuEtchRateMask:.3e} μm/s")
-print(f"Poly etching rate (ion, neutral): {ionEtchRatePoly:.3e} μm/s, {neuEtchRatePoly:.3e} μm/s")
-print(f"Subs etching rate (ion, neutral): {ionEtchRateSubs:.3e} μm/s, {neuEtchRateSubs:.3e} μm/s")
+print(
+    f"Mask etching rate (ion, neutral): {ionEtchRateMask:.3e} μm/s, {neuEtchRateMask:.3e} μm/s"
+)
+print(
+    f"Poly etching rate (ion, neutral): {ionEtchRatePoly:.3e} μm/s, {neuEtchRatePoly:.3e} μm/s"
+)
+print(
+    f"Subs etching rate (ion, neutral): {ionEtchRateSubs:.3e} μm/s, {neuEtchRateSubs:.3e} μm/s"
+)
 
 
 def rateFunctionDep(fluxes, material):
@@ -119,8 +127,7 @@ depoProcess.setTimeStepRatio(0.2)
 etchProcess = vps.gpu.Process(context, geometry, etchModel, params["etchTime"])
 etchProcess.setTimeStepRatio(0.2)
 
-geometry.saveSurfaceMesh("initial.vtp")
-geometry.saveVolumeMesh("initial")
+geometry.saveSurfaceMesh("initial.vtp", True)
 
 geometry.duplicateTopLevelSet(vps.Material.Polymer)
 
@@ -130,15 +137,17 @@ print(f"Starting {numCycles} cycles")
 for i in range(numCycles):
     print(f"Cycle {i + 1}")
     # geometry.saveLevelSetMesh(filename=f"run_{2*i}", width=3)
-    geometry.saveSurfaceMesh(f"run_{2 * i}.vtp")
-    # geometry.saveVolumeMesh(f"run_{2*i}")
+    geometry.saveSurfaceMesh(f"run_{2 * i}.vtp", True)
+    geometry.saveHullMesh(f"run_{2 * i}", 0.05)
+
     print(f"  - Deposition -")
     depoProcess.apply()
     # geometry.saveLevelSetMesh(filename=f"run_{2*i+1}", width=3)
-    geometry.saveSurfaceMesh(f"run_{2 * i + 1}.vtp")
-    # geometry.saveVolumeMesh(f"run_{2*i+1}")
+    geometry.saveSurfaceMesh(f"run_{2 * i + 1}.vtp", True)
+    geometry.saveHullMesh(f"run_{2*i+1}", 0.05)
+
     print(f"  - Etching -")
     etchProcess.apply()
 
-geometry.saveSurfaceMesh(f"final.vtp")
+geometry.saveSurfaceMesh(f"final.vtp", True)
 geometry.saveVolumeMesh(f"final")
