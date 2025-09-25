e77b98af42dc9e6ba10b0a14e97ee48177170dd3
Tobias Reiter <t.reiter1@live.de>
2025-09-25 11:09:10 +0200

    Adjust stack etching example

--- examples/stackEtching/stackEtching.py
+++ examples/stackEtching/stackEtching.py
@@ -36,14 +36,47 @@ vps.d2.MakeStack(
 
 geometry.duplicateTopLevelSet(vps.Material.Polymer)
 
-model = vps.d2.FluorocarbonEtching(
-    ionFlux=params["ionFlux"],
-    etchantFlux=params["etchantFlux"],
-    polyFlux=params["polyFlux"],
-    meanIonEnergy=params["meanIonEnergy"],
-    sigmaIonEnergy=params["sigmaIonEnergy"],
-    ionExponent=params["ionExponent"],
-)
+parameters = vps.FluorocarbonParameters()
+
+matSi = vps.FluorocarbonMaterialParameters()
+matSi.id = vps.Material.Si
+matSi.density = 5.5
+
+matSiO2 = vps.FluorocarbonMaterialParameters()
+matSiO2.id = vps.Material.SiO2
+matSiO2.density = 2.2
+
+matSi3N4 = vps.FluorocarbonMaterialParameters()
+matSi3N4.id = vps.Material.Si3N4
+matSi3N4.density = 2.3
+
+matPoly = vps.FluorocarbonMaterialParameters()
+matPoly.id = vps.Material.Polymer
+matPoly.density = 2.0
+matPoly.beta_e = 0.6
+matPoly.A_ie = 0.0361 * 2
+
+matMask = vps.FluorocarbonMaterialParameters()
+matMask.id = vps.Material.Mask
+matMask.density = 500.0
+matMask.beta_e = 0.1
+matMask.beta_p = 0.01
+matMask.Eth_sp = 20.0
+
+parameters.addMaterial(matSi)
+parameters.addMaterial(matSiO2)
+parameters.addMaterial(matSi3N4)
+parameters.addMaterial(matPoly)
+parameters.addMaterial(matMask)
+
+parameters.ionFlux = params["ionFlux"]
+parameters.etchantFlux = params["etchantFlux"]
+parameters.polyFlux = params["polyFlux"]
+parameters.Ions.meanEnergy = params["meanIonEnergy"]
+parameters.Ions.sigmaEnergy = params["sigmaIonEnergy"]
+parameters.Ions.exponent = params["ionExponent"]
+
+model = vps.d2.FluorocarbonEtching(parameters)
 
 covParams = vps.CoverageParameters()
 covParams.maxIterations = 10
