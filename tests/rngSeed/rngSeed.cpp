3d47630780cd1e327e92c65b1101896770b2f40e
Tobias Reiter <t.reiter1@live.de>
2025-12-05 16:01:45 +0100

    Test 2d and 3d

--- tests/rngSeed/rngSeed.cpp
+++ tests/rngSeed/rngSeed.cpp
@@ -13,11 +13,8 @@
 
 using namespace viennaps;
 
-int main() {
+template <typename T, int D> void testRngSeed() {
   Logger::setLogLevel(LogLevel::WARNING);
-  constexpr int D = 3;
-  using T = double;
-
   std::vector<FluxEngineType> engineTypes = {FluxEngineType::CPU_DISK,
                                              FluxEngineType::CPU_TRIANGLE};
 #ifdef VIENNACORE_COMPILE_GPU
@@ -26,22 +23,20 @@ int main() {
 #endif
 
   auto model = SmartPointer<SingleParticleProcess<T, D>>::New(1.0, 1.0, 1.0);
+  auto domain = Domain<T, D>::New(1.0, 10.0, 10.0);
+  MakePlane<T, D>(domain, 0.0).apply();
+
+  RayTracingParameters rayParams;
+  rayParams.raysPerPoint = 100;
+  rayParams.rngSeed = 42;
+  rayParams.useRandomSeeds = false; // it is important to disable random seeds
+                                    // here, also when rngSeed is set
 
   for (const auto &engineType : engineTypes) {
     std::cout << "Testing rng seed: " << to_string(engineType) << " in " << D
               << "D" << std::endl;
-    auto domain = Domain<T, D>::New(1.0, 10.0, 10.0);
-
-    MakePlane<T, D>(domain, 0.0).apply();
-
-    RayTracingParameters rayParams;
-    rayParams.raysPerPoint = 100;
-    rayParams.rngSeed = 42;
-    rayParams.useRandomSeeds = false; // it is important to disable random seeds
-                                      // here, also when rngSeed is set
 
     std::vector<T> flux1, flux2;
-
     {
       Process<T, D> process(domain, model);
       process.setFluxEngineType(engineType);
@@ -64,4 +59,9 @@ int main() {
     for (size_t i = 0; i < flux1.size(); ++i)
       VC_TEST_ASSERT(flux1[i] == flux2[i]);
   }
+}
+
+int main() {
+  testRngSeed<double, 2>();
+  testRngSeed<double, 3>();
 }
