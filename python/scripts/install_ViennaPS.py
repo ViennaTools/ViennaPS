e6722db509f73edbcbcc4b26cfac4090381884f9
filipovic <filipovic@iue.tuwien.ac.at>
2026-07-20 23:32:03 +0200

    update version and LS dependency version

--- python/scripts/install_ViennaPS.py
+++ python/scripts/install_ViennaPS.py
@@ -9,7 +9,7 @@ import tempfile
 from pathlib import Path
 
 REQUIRED_NVCC_MAJOR = 12
-DEFAULT_VIENNALS_VERSION = "5.8.3"
+DEFAULT_VIENNALS_VERSION = "5.8.5"
 
 # Detect OS
 IS_WINDOWS = sys.platform == "win32" or os.name == "nt"
