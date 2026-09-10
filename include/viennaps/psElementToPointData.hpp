477c1aa4199e72531a51eb5287ab82ca5d946499
Tobias Reiter <t.reiter1@live.de>
2026-09-10 16:39:33 +0200

    refactor: element to point data logic

--- include/viennaps/psElementToPointData.hpp
+++ include/viennaps/psElementToPointData.hpp
@@ -28,13 +28,13 @@ class ElementToPointData {
   struct CloseElements {
     std::vector<size_t> indices;
     std::vector<double> weights;
-    unsigned numClosePoints;
   };
 
   std::vector<std::vector<ResultType>> elementDataArrays_;
   std::vector<CloseElements> closeElements_;
   static constexpr bool discard2 = d2;
   static constexpr bool discard4 = d4;
+  static constexpr double weightThreshold = 1e-4;
 
 public:
   ElementToPointData() = default;
@@ -112,22 +112,25 @@ public:
               ->findNearestWithinRadius(points[i], lookupRadiusSquared_)
               .value();
 
-      std::vector<double> weights(closeElements.size(), 0.);
-      std::vector<size_t> elementIndices(closeElements.size(), 0);
+      std::vector<double> weights;
+      std::vector<size_t> elementIndices;
+      weights.reserve(closeElements.size());
+      elementIndices.reserve(closeElements.size());
 
       // compute weights based on normal alignment
       unsigned numClosePoints = 0;
       for (std::size_t n = 0; n < closeElements.size(); ++n) {
-        const auto &p = closeElements[n];
-        assert(p.first < numElements);
+        const auto &[idx, distanceSquared] = closeElements[n];
+        assert(idx < numElements);
+        assert(distanceSquared < lookupRadiusSquared_ + 1e-6);
 
         const double weight =
-            DotProductNT(normals->at(i), elementNormals->at(p.first));
+            DotProductNT(normals->at(i), elementNormals->at(idx)) +
+            (lookupRadiusSquared_ - distanceSquared) / lookupRadiusSquared_;
 
-        if (weight > 1e-6 && !std::isnan(weight)) {
-          weights[n] = weight;
-          elementIndices[n] = p.first;
-          ++numClosePoints;
+        if (!std::isnan(weight) && weight > weightThreshold) {
+          weights.push_back(weight);
+          elementIndices.push_back(idx);
         }
       }
 
@@ -135,7 +138,6 @@ public:
              "Weights and element indices should not be empty here.");
       closeElements_[i].indices = std::move(elementIndices);
       closeElements_[i].weights = std::move(weights);
-      closeElements_[i].numClosePoints = numClosePoints;
     }
   }
 
@@ -153,7 +155,7 @@ public:
 #pragma omp parallel for schedule(static)
     for (unsigned i = 0; i < numPoints; i++) {
 
-      const auto &[indices, weights, numClosePoints] = closeElements_[i];
+      const auto &[indices, weights] = closeElements_[i];
 
       for (unsigned j = 0; j < numData; ++j) {
         NumericType value = NumericType(0);
@@ -161,18 +163,16 @@ public:
         auto pointData = pointData_->getScalarData(j);
 
         // Discard outlier values if enabled
-        const auto weightsCopy =
-            discardOutliers(weights, indices, elementData, numClosePoints);
+        const auto weightsCleaned =
+            discardOutliers(weights, indices, elementData);
 
         // Compute weighted average
-        const double sum =
-            std::accumulate(weightsCopy.cbegin(), weightsCopy.cend(), 0.0);
+        const double sum = std::accumulate(weightsCleaned.cbegin(),
+                                           weightsCleaned.cend(), 0.0);
 
         if (sum > 1e-6) [[likely]] {
           for (size_t k = 0; k < indices.size(); ++k) {
-            if (weightsCopy[k] > 0.0) {
-              value += weightsCopy[k] * elementData[indices[k]];
-            }
+            value += weightsCleaned[k] * elementData[indices[k]];
           }
           value /= sum;
         } else {
@@ -237,36 +237,33 @@ private:
   };
 
   static MinMaxInfo
-  findMinMaxValues(const std::vector<double> &weights,
-                   const std::vector<size_t> &closePoints,
+  findMinMaxValues(const std::vector<size_t> &closePoints,
                    const std::vector<ResultType> &elementData) {
     MinMaxInfo info;
 
     for (std::size_t k = 0; k < closePoints.size(); ++k) {
-      if (weights[k] > 0.0) {
-        const auto value = elementData[closePoints[k]];
+      const auto value = elementData[closePoints[k]];
 
-        // Update min values
-        if (value < info.min1) {
-          info.min2 = info.min1;
-          info.minIdx2 = info.minIdx1;
-          info.min1 = value;
-          info.minIdx1 = static_cast<int>(k);
-        } else if (value < info.min2) {
-          info.min2 = value;
-          info.minIdx2 = static_cast<int>(k);
-        }
+      // Update min values
+      if (value < info.min1) {
+        info.min2 = info.min1;
+        info.minIdx2 = info.minIdx1;
+        info.min1 = value;
+        info.minIdx1 = static_cast<int>(k);
+      } else if (value < info.min2) {
+        info.min2 = value;
+        info.minIdx2 = static_cast<int>(k);
+      }
 
-        // Update max values
-        if (value > info.max1) {
-          info.max2 = info.max1;
-          info.maxIdx2 = info.maxIdx1;
-          info.max1 = value;
-          info.maxIdx1 = static_cast<int>(k);
-        } else if (value > info.max2) {
-          info.max2 = value;
-          info.maxIdx2 = static_cast<int>(k);
-        }
+      // Update max values
+      if (value > info.max1) {
+        info.max2 = info.max1;
+        info.maxIdx2 = info.maxIdx1;
+        info.max1 = value;
+        info.maxIdx1 = static_cast<int>(k);
+      } else if (value > info.max2) {
+        info.max2 = value;
+        info.maxIdx2 = static_cast<int>(k);
       }
     }
 
@@ -275,24 +272,24 @@ private:
 
   static auto discardOutliers(const std::vector<double> &weights,
                               const std::vector<size_t> &closePoints,
-                              const std::vector<ResultType> &elementData,
-                              unsigned numClosePoints) {
+                              const std::vector<ResultType> &elementData) {
 
     // copy weights to modify
     auto weightsCopy = weights;
 
-    if (discard4 && numClosePoints > 4) {
-      const auto info = findMinMaxValues(weights, closePoints, elementData);
+    if (discard4 && closePoints.size() > 4) {
+      const auto info = findMinMaxValues(closePoints, elementData);
 
       if (info.maxIdx1 != -1 && info.maxIdx2 != -1 && info.minIdx1 != -1 &&
           info.minIdx2 != -1) {
+        // settting to 0 == discarding the weight
         weightsCopy[info.minIdx1] = 0.0;
         weightsCopy[info.minIdx2] = 0.0;
         weightsCopy[info.maxIdx1] = 0.0;
         weightsCopy[info.maxIdx2] = 0.0;
       }
-    } else if (discard2 && numClosePoints > 2) {
-      const auto info = findMinMaxValues(weights, closePoints, elementData);
+    } else if (discard2 && closePoints.size() > 2) {
+      const auto info = findMinMaxValues(closePoints, elementData);
 
       if (info.minIdx1 != -1 && info.maxIdx1 != -1) {
         weightsCopy[info.minIdx1] = 0.0;
